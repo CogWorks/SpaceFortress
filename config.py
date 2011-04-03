5cf659673127d6ba430f9990d4607b2136704472
Ryan Hope <rmh3093@gmail.com>
2011-04-03 17:42:52 -0400

    start gui config editor

--- config.py
+++ config.py
@@ -1,14 +1,33 @@
-import json, copy
+#!/usr/bin/env python
+
+import sys, json, copy
+from pythonutils.odict import OrderedDict
+from PySide.QtCore import *
+from PySide.QtGui import *
+
+CT_LINEEDIT = 0
+CT_COMBO = 1
+CT_CHECKBOX = 2
+CT_SPINBOX = 3
+CT_DBLSPINBOX = 4
+
+PYGAME_KEYS = ['1','2','3','4','5','6','7','8','9','0',
+               'q','w','e','r','t','y','u','i','o','p',
+               'a','s','d','f','g','h','j','k','l',
+               'z','x','c','v','b','n','m','SPACE',
+               'BACKSPACE','TAB','RETURN','COMMA','MINUS','PERIOD',
+               'SLASH','SEMICOLON','QUOTE','LEFTBRACKET','RIGHTBRACKET',
+               'BACKSLASH','EQUALS','BACKQUOTE','UP','DOWN','RIGHT','LEFT']
 
 class Config():
     
-    config = {}
+    config = OrderedDict()
     
     def add_category(self, category):
         if not self.config.has_key(category):
-            self.config[category] = {}
+            self.config[category] = OrderedDict()
             
-    def add_setting(self, category, setting, value, about=''):
+    def add_setting(self, category, setting, value, about='', type=CT_SPINBOX, **kwargs):
         assert category!=None, 'Must specify a category'
         assert setting!=None, 'Must specify a setting'
         assert value!=None, 'Must specify a value'
@@ -17,33 +36,47 @@ class Config():
             self.config[category][setting] = {}
         self.config[category][setting]['value'] = value
         self.config[category][setting]['about'] = about
+        self.config[category][setting]['type'] = type
+        for k in kwargs:
+            self.config[category][setting][k] = kwargs[k]
         
-    def get_setting(self, category, setting):
+    def get_setting(self, category, setting, complete=False):
         assert category!=None, 'Must specify a category'
         assert setting!=None, 'Must specify a setting'
         assert self.config.has_key(category), 'Category does not exist'
         assert self.config[category].has_key(setting), 'Setting in category does not exist'
         assert self.config[category][setting].has_key('value'), 'Setting in category has no value'
-        return self.config[category][setting]['value']
+        if complete:
+            return self.config[category][setting]
+        else:
+            return self.config[category][setting]['value']
+    
+    def get_settings(self, category):
+        assert category!=None, 'Must specify a category'
+        assert self.config.has_key(category), 'Category does not exist'
+        return self.config[category].keys()
+    
+    def get_categories(self):
+        return self.config.keys()
         
     def __init__(self):
         
-        self.add_setting('General', 'human', True, 'Will this game be played by a human or a cognitive model? Set to f to disable human control, enabling ACT-R')
-        self.add_setting('General', 'fullscreen', False, 'Run at full screen? Set to f to run in a window')
+        self.add_setting('General', 'human', True, type=CT_CHECKBOX, about='Will this game be played by a human or a cognitive model? Set to f to disable human control, enabling ACT-R')
+        self.add_setting('General', 'fullscreen', False, type=CT_CHECKBOX, about='Run at full screen? Set to f to run in a window')
         self.add_setting('General', 'linewidth', 1, 'Width of lines drawn on screen. Increase for a more "projector-friendly" game')
-        self.add_setting('General', 'psf', False, 'Original PSF (SF4) settings? If t, *ALL* further values are ignored')
+        self.add_setting('General', 'psf', False, type=CT_CHECKBOX, about='Original PSF (SF4) settings? If t, *ALL* further values are ignored')
         self.add_setting('General', 'game_time', 30000, 'Time in milliseconds for a game. NOTE! If you escape in the middle of a game, the log will have "short" prepended to the name')
         self.add_setting('General', 'games_per_session', 8, 'Number of games per "session"')
-        self.add_setting('General', 'print_events', False, 'Print events to stdout')
-        self.add_setting('General', 'sound', True, 'Enable/disable sound')
+        self.add_setting('General', 'print_events', False, type=CT_CHECKBOX, about='Print events to stdout')
+        self.add_setting('General', 'sound', True, type=CT_CHECKBOX, about='Enable/disable sound')
         
-        self.add_setting('Keybindings', 'thrust_key', 'w')
-        self.add_setting('Keybindings', 'left_turn_key', 'a')
-        self.add_setting('Keybindings', 'right_turn_key', 'd')
-        self.add_setting('Keybindings', 'fire_key', 'SPACE')
-        self.add_setting('Keybindings', 'IFF_key', 'j')
-        self.add_setting('Keybindings', 'shots_key', 'k')
-        self.add_setting('Keybindings', 'pnts_key', 'l')
+        self.add_setting('Keybindings', 'thrust_key', 'w', type=CT_COMBO, options=PYGAME_KEYS)
+        self.add_setting('Keybindings', 'left_turn_key', 'a', type=CT_COMBO, options=PYGAME_KEYS)
+        self.add_setting('Keybindings', 'right_turn_key', 'd', type=CT_COMBO, options=PYGAME_KEYS)
+        self.add_setting('Keybindings', 'fire_key', 'SPACE', type=CT_COMBO, options=PYGAME_KEYS)
+        self.add_setting('Keybindings', 'IFF_key', 'j', type=CT_COMBO, options=PYGAME_KEYS)
+        self.add_setting('Keybindings', 'shots_key', 'k', type=CT_COMBO, options=PYGAME_KEYS)
+        self.add_setting('Keybindings', 'pnts_key', 'l', type=CT_COMBO, options=PYGAME_KEYS)
         
         self.add_setting('Ship', 'ship_acceleration', 0.3, 'Ship acceleration factor')
         self.add_setting('Ship', 'ship_hit_points', 4, 'Number of hits ship takes before it is destroyed')
@@ -66,35 +99,35 @@ class Config():
         self.add_setting('Hexagon', 'hex_pos_y', 315, 'Hex y position')
         self.add_setting('Hexagon', 'big_hex', 200, '"Radius" of large hexagon')
         self.add_setting('Hexagon', 'small_hex', 40, '"Radius" of small hexagon')
-        self.add_setting('Hexagon', 'hex_shrink', False, 'Does the outer hex shrink over time?')
+        self.add_setting('Hexagon', 'hex_shrink', False, type=CT_CHECKBOX, about='Does the outer hex shrink over time?')
         self.add_setting('Hexagon', 'hex_shrink_radius', 150, 'If hex shrinks, to what minimum radius?')
         
-        self.add_setting('Fortress', 'fortress_exists', True, 'Does the fortress exist?')
+        self.add_setting('Fortress', 'fortress_exists', True, type=CT_CHECKBOX, about='Does the fortress exist?')
         self.add_setting('Fortress', 'fortress_lock_time', 1000, 'Time in milliseconds it takes the fortress to lock on to the ship before firing')
         self.add_setting('Fortress', 'fortress_pos_x', 355, 'Fortress x position')
         self.add_setting('Fortress', 'fortress_pos_y', 315, 'Fortress y position')
         self.add_setting('Fortress', 'fortress_radius', 18, 'Fortress collision radius')
         self.add_setting('Fortress', 'fortress_sector_size', 10, 'Fortress "sector size." Angle of tolerance before fortress turns to face ship. Bigger means fortress fires more often')
-        self.add_setting('Fortress', 'hit_fortress_while_mine', False, "Can the fortress be hit if there's a mine onscreen?")
+        self.add_setting('Fortress', 'hit_fortress_while_mine', False, type=CT_CHECKBOX, about="Can the fortress be hit if there's a mine onscreen?")
         self.add_setting('Fortress', 'vlner_threshold', 10, 'Minimum vulnerability before you can destroy the fortress with a double shot. Note - keep in mind that the first shot of a "double shot" will increase VLNER')
         self.add_setting('Fortress', 'vlner_time', 250, 'Time in milliseconds that must pass between shots to avoid the fortress vulnerability to reset')
 
         self.add_setting('Shell', 'shell_speed', 6, 'Speed of shell fired from fortress')
         self.add_setting('Shell', 'shell_radius', 3, 'Shell collision radius')
         
-        self.add_setting('Bonus', 'bonus_exists', True, 'Do bonuses exist?')
-        self.add_setting('Bonus', 'bonus_system', "AX-CPT", 'Bonus system standard or AX-CPT?')
-        self.add_setting('Bonus', 'randomize_bonus_pos', False, 'Randomize bonus position?')
+        self.add_setting('Bonus', 'bonus_exists', True, type=CT_CHECKBOX, about='Do bonuses exist?')
+        self.add_setting('Bonus', 'bonus_system', "AX-CPT", type=CT_LINEEDIT, about='Bonus system standard or AX-CPT?')
+        self.add_setting('Bonus', 'randomize_bonus_pos', False, type=CT_CHECKBOX, about='Randomize bonus position?')
         self.add_setting('Bonus', 'bonus_pos_x', 355, 'Bonus x position')
         self.add_setting('Bonus', 'bonus_pos_y', 390, 'Bonus y position')
         self.add_setting('Bonus', 'bonus_probability', 0.3, 'Probability that next symbol will be the bonus symbol')
-        self.add_setting('Bonus', 'bonus_symbol', '$', 'Bonus symbol')
-        self.add_setting('Bonus', 'non_bonus_symbols', ['!', '&', '*', '%', '@'], "Non-bonus symbols. Defaults are # & * % @. Don't use '-', because that's used in the log file to represent that there's no symbol present")
+        self.add_setting('Bonus', 'bonus_symbol', '$', type=CT_LINEEDIT, about='Bonus symbol')
+        self.add_setting('Bonus', 'non_bonus_symbols', ['!', '&', '*', '%', '@'], type=CT_LINEEDIT, about="Non-bonus symbols. Defaults are # & * % @. Don't use '-', because that's used in the log file to represent that there's no symbol present")
         self.add_setting('Bonus', 'symbol_down_time', 833, '"Blank time" between symbol appearances in milliseconds**. (Seems like a weird number, but it\'s to sync with the frame-based original)')
         self.add_setting('Bonus', 'symbol_up_time', 2500, 'Time in milliseconds each symbol is visible**')
 
-        self.add_setting('Mine', 'mine_exists', True, 'Do mines exists?')
-        self.add_setting('Mine', 'mine_mode', 'standard', 'Set mine behavior to standard or MOT')
+        self.add_setting('Mine', 'mine_exists', True, type=CT_CHECKBOX, about='Do mines exists?')
+        self.add_setting('Mine', 'mine_mode', 'standard', type=CT_LINEEDIT, about='Set mine behavior to standard or MOT')
         self.add_setting('Mine', 'num_foes', 3, 'Number of foe mines')
         self.add_setting('Mine', 'mine_radius', 20, 'Mine collision radius')
         self.add_setting('Mine', 'mine_speed', 3, 'Mine speed')
@@ -105,20 +138,20 @@ class Config():
         self.add_setting('Mine', 'mine_probability', 0.3, 'Probability that next mine will be a foe')
         self.add_setting('Mine', 'mine_timeout', 10000, 'Time in milliseconds for a mine to timeout and disappear')
         self.add_setting('Mine', 'mine_spawn', 5000, 'Time in milliseconds for a mine to spawn')
-        self.add_setting('Mine', 'fortress_resets_mine', True, 'Does destroying the fortress reset the mine timer?')
+        self.add_setting('Mine', 'fortress_resets_mine', True, type=CT_CHECKBOX, about='Does destroying the fortress reset the mine timer?')
         
         self.add_setting('MOT', 'MOT_count', 5, 'Number of mines')
         self.add_setting('MOT', 'MOT_identification_time', 5000, 'Time to allow for MOT identification')
-        self.add_setting('MOT', 'MOT_identification_type', 'shots', 'MOT identification through shots or ship? (i.e., fly into the mine)')
+        self.add_setting('MOT', 'MOT_identification_type', 'shots', type=CT_LINEEDIT, about='MOT identification through shots or ship? (i.e., fly into the mine)')
         self.add_setting('MOT', 'MOT_max_deflection', 15, 'Angle in degrees for the maximum amount a mine can change direction')
         self.add_setting('MOT', 'MOT_move_time', 5000, 'Time to move mines with IFF info hidden')
-        self.add_setting('MOT', 'MOT_movement_style', 'bounce', 'Do MOT mines bounce off walls or warp around edges?')
+        self.add_setting('MOT', 'MOT_movement_style', 'bounce', type=CT_LINEEDIT, about='Do MOT mines bounce off walls or warp around edges?')
         self.add_setting('MOT', 'MOT_off_time', 5000, 'Time in between MOT "cycles"')
         self.add_setting('MOT', 'MOT_onset_time', 3000, 'Time to display "frozen" MOT mines on onset')
         self.add_setting('MOT', 'MOT_switch_time', 1000, 'Time in between direction changes while MOT mines are moving')
 
-        self.add_setting('Score', 'new_scoring', False, 'Use the new scoring system for Flight, Fortress, and Mines? (instead of PNTS, CNTRL, VLCTY and SPEED)')
-        self.add_setting('Score', 'new_scoring_pos', True, 'New scoring position is a more "eye-tracker friendly" format that places the scores around the perimeter of the gameworld, rather than just all along the bottom. Set to f for default positioning')
+        self.add_setting('Score', 'new_scoring', False, type=CT_CHECKBOX, about='Use the new scoring system for Flight, Fortress, and Mines? (instead of PNTS, CNTRL, VLCTY and SPEED)')
+        self.add_setting('Score', 'new_scoring_pos', True, type=CT_CHECKBOX, about='New scoring position is a more "eye-tracker friendly" format that places the scores around the perimeter of the gameworld, rather than just all along the bottom. Set to f for default positioning')
         self.add_setting('Score', 'PNTS_pos', 1, 'Set positions for the display elements. 1 = leftmost for original scoring position for new scoring position, 1 = left item on top row, proceeds clockwise PNTS position(or Flight, for new)')
         self.add_setting('Score', 'CNTRL_pos', 2, 'CNTRL position(or Fortress, for new)')
         self.add_setting('Score', 'VLCTY_pos', 3, 'VLCTY position(or Mines, for new)')
@@ -161,9 +194,95 @@ def gen_config(config_file):
     except IOError:
         ret = False
     return ret
+    
+class ConfigEditor(QMainWindow):
+    
+    def __init__(self):
+        super(ConfigEditor, self).__init__()
+        
+        self.config = Config()
+        
+        self.categories = QListWidget()
+        self.categories.setFocusPolicy(Qt.NoFocus)
+        self.settings = QStackedWidget()
+        
+        self.setFixedSize(640,420)
+        self.categories.setMaximumWidth(120)
+        
+        QObject.connect(self.categories, SIGNAL('itemSelectionChanged()'), self.category_selected)
+        
+        for cat in self.config.get_categories():
+            self.categories.addItem(cat)
+            settings_layout = QGridLayout()
+            r = 0
+            c = 0
+            for setting in self.config.get_settings(cat):
+                info = self.config.get_setting(cat, setting, True)
+                s = QWidget()
+                sl = QVBoxLayout()
+                sl.setAlignment(Qt.AlignLeft)
+                sl.addWidget(QLabel(setting))
+                if info['type'] == CT_LINEEDIT:
+                    w = QLineEdit()
+                    w.setText(str(info['value']))
+                    if info.has_key('n'):
+                        w.setMaxLength(info['n'])
+                        print w.minimumSizeHint()
+                        w.setFixedWidth(info['n']*w.minimumSizeHint().height())
+                elif info['type'] == CT_CHECKBOX:
+                    w = QCheckBox()
+                    if info['value']:
+                        w.setCheckState(Qt.Checked)
+                    else:
+                        w.setCheckState(Qt.Unchecked)
+                elif info['type'] == CT_SPINBOX:
+                    w = QSpinBox()
+                    w.setValue(info['value'])
+                elif info['type'] == CT_COMBO:
+                    w = QComboBox()
+                    if info.has_key('options'):
+                        w.addItems(info['options'])
+                        for i in range(0,len(info['options'])):
+                            if info['options'][i] == info['value']:
+                                w.setCurrentIndex(i)
+                sl.addWidget(w)
+                s.setLayout(sl)
+                c = self.config.config[cat].index(setting) % 2
+                settings_layout.addWidget(s, r, c)
+                if c == 1:
+                    r += 1
+            settings = QWidget()
+            settings.setLayout(settings_layout)
+            settings_scroller = QScrollArea()
+            settings_scroller.setAlignment(Qt.AlignHCenter)
+            settings_scroller.setWidget(settings)
+            self.settings.addWidget(settings_scroller)
+        
+        self.main = QWidget()
+        self.main_layout = QHBoxLayout()
+        self.main_layout.addWidget(self.categories)
+        self.main_layout.addWidget(self.settings)
+        self.main.setLayout(self.main_layout)
+        
+        self.setCentralWidget(self.main)
+        self.setWindowTitle('Spacefortress Config Editor')
+        self.setUnifiedTitleAndToolBarOnMac(True)
+        
+        self.categories.setCurrentItem(self.categories.item(0))
+        
+        self.show()
+        self.activateWindow()
+        self.raise_()
+        
+        screen = QDesktopWidget().screenGeometry()
+        size =  self.geometry()
+        self.move((screen.width()-size.width())/2, (screen.height()-size.height())/2)
+        
+    def category_selected(self):
+        self.settings.setCurrentIndex(self.config.config.index(self.categories.selectedItems()[0].text()))
 
 if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    editor = ConfigEditor()
+    sys.exit(app.exec_())
     
-    config = Config()
-    print config.config
-    gen_config("/tmp/config")
