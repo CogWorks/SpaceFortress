30665e47ada0add5ff4d4724f603f8a24d452821
Ryan Hope <rmh3093@gmail.com>
2011-10-11 10:26:22 -0400

    update pycogworks, disable SystemProfile on non mac

--- Plugins/SystemProfile.py
+++ Plugins/SystemProfile.py
@@ -21,20 +21,22 @@ class SF5Plugin(object):
     
     def eventCallback(self, *args, **kwargs):
         
-        if args[3] == 'config' and args[4] == 'load':
-            if args[5] == 'defaults':
-                if sys.platform == 'darwin':
-                    self.app.config.add_setting('SystemProfile', 'log_mac_hw', False, type=2)
-                
-        elif args[3] == 'log':
-    
-            if args[4] == 'header' and args[5] == 'ready':
-                
-                if self.app.config.get_setting('SystemProfile','log_mac_hw'):
-                    info = getHWInfo()
-                    self.app.gameevents.add("exp_hw", "model", info['Model Identifier'], type='EVENT_SYSTEM')
-                    self.app.gameevents.add("exp_hw", "processor", info['Processor Name'], type='EVENT_SYSTEM')
-                    self.app.gameevents.add("exp_hw", "processors", info['Number Of Processors'], type='EVENT_SYSTEM')
-                    self.app.gameevents.add("exp_hw", "cores", info['Total Number Of Cores'], type='EVENT_SYSTEM')
-                    self.app.gameevents.add("exp_hw", "cpu_speed", info['Processor Speed'], type='EVENT_SYSTEM')
-                    self.app.gameevents.add("exp_hw", "memory", info['Memory'], type='EVENT_SYSTEM')
+        if sys.platform == "darwin":
+            
+            if args[3] == 'config' and args[4] == 'load':
+                if args[5] == 'defaults':
+                    if sys.platform == 'darwin':
+                        self.app.config.add_setting('SystemProfile', 'log_mac_hw', False, type=2)
+                    
+            elif args[3] == 'log':
+        
+                if args[4] == 'header' and args[5] == 'ready':
+                    
+                    if self.app.config.get_setting('SystemProfile','log_mac_hw'):
+                        info = getHWInfo()
+                        self.app.gameevents.add("exp_hw", "model", info['Model Identifier'], type='EVENT_SYSTEM')
+                        self.app.gameevents.add("exp_hw", "processor", info['Processor Name'], type='EVENT_SYSTEM')
+                        self.app.gameevents.add("exp_hw", "processors", info['Number Of Processors'], type='EVENT_SYSTEM')
+                        self.app.gameevents.add("exp_hw", "cores", info['Total Number Of Cores'], type='EVENT_SYSTEM')
+                        self.app.gameevents.add("exp_hw", "cpu_speed", info['Processor Speed'], type='EVENT_SYSTEM')
+                        self.app.gameevents.add("exp_hw", "memory", info['Memory'], type='EVENT_SYSTEM')
