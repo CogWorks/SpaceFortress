82595b6c56091fae520201d17204490462a1b42d
Ryan Hope <rmh3093@gmail.com>
2012-07-04 10:52:41 -0400

    make sure that plugins dont block game starting

--- Plugins/CWSubject.py
+++ Plugins/CWSubject.py
@@ -7,49 +7,53 @@ import sys
 from PySide.QtCore import *
 from PySide.QtGui import *
 
-from pycogworks.cwsubject import * 
-from pycogworks.util import rin2id
+try:
+    from pycogworks.cwsubject import *
+    from pycogworks.util import rin2id
 
-class SF5Plugin(object):
-
-    def __init__(self, app):
-        super(SF5Plugin, self).__init__()
-        self.app = app
-        self.subjectInfo = None
-        self.expRoom = None
-
-    def eventCallback(self, *args, **kwargs):
-
-        if args[3] == 'config' and args[4] == 'load':
-
-            if args[5] == 'defaults':
-
-                self.app.config.add_setting('CogWorks Exp', 'subject_window', False, type=2, about='Prompt for subject information')
-                self.app.config.add_setting('CogWorks Exp', 'experiment_room', '', type=3, about='Write history file')
-
-            elif args[5] == 'user':
-
-                if self.app.config.get_setting('CogWorks Exp','subject_window'):
-
-                    self.subjectInfo = getSubjectInfo(minimal=True)
-                    if self.subjectInfo:
-                        self.app.config.update_setting_value("General","id",rin2id(self.subjectInfo['rin'])[:8])
-                    else:
-                        sys.exit()
+    class SF5Plugin(object):
+    
+        def __init__(self, app):
+            super(SF5Plugin, self).__init__()
+            self.app = app
+            self.subjectInfo = None
+            self.expRoom = None
+    
+        def eventCallback(self, *args, **kwargs):
             
-                self.expRoom = self.app.config.get_setting('CogWorks Exp','experiment_room').strip()
+            if args[3] == 'config' and args[4] == 'load':
     
-        elif args[3] == 'log':
+                if args[5] == 'defaults':
     
-            if args[4] == 'basename' and args[5] == 'ready':
-                if self.app.config.get_setting('CogWorks Exp','subject_window') and self.subjectInfo:
-                    eid = rin2id( self.subjectInfo['rin'] )
-                    self.subjectInfo['encrypted_rin'] = eid
-                    self.subjectInfo['cipher'] = 'AES/CBC (RIJNDAEL) - 16Byte Key'
-                    writeHistoryFile( self.app.log_basename, self.subjectInfo )
+                    self.app.config.add_setting('CogWorks Exp', 'subject_window', False, type=2, about='Prompt for subject information')
+                    self.app.config.add_setting('CogWorks Exp', 'experiment_room', '', type=3, about='Write history file')
     
-            elif args[4] == 'header' and args[5] == 'ready':
-                if self.app.config.get_setting('CogWorks Exp','subject_window'):
-                    self.app.gameevents.add("participant", "encrypted_rin", self.subjectInfo['encrypted_rin'], type='EVENT_SYSTEM')
-                if self.expRoom and len(self.expRoom) > 0:
-                    self.app.gameevents.add("experiment", "room", self.expRoom, type='EVENT_SYSTEM')
+                elif args[5] == 'user':
+    
+                    if self.app.config.get_setting('CogWorks Exp','subject_window'):
+    
+                        self.subjectInfo = getSubjectInfo(minimal=True)
+                        if self.subjectInfo:
+                            self.app.config.update_setting_value("General","id",rin2id(self.subjectInfo['rin'])[:8])
+                        else:
+                            sys.exit()
+                
+                    self.expRoom = self.app.config.get_setting('CogWorks Exp','experiment_room').strip()
+        
+            elif args[3] == 'log':
+        
+                if args[4] == 'basename' and args[5] == 'ready':
+                    if self.app.config.get_setting('CogWorks Exp','subject_window') and self.subjectInfo:
+                        eid = rin2id( self.subjectInfo['rin'] )
+                        self.subjectInfo['encrypted_rin'] = eid
+                        self.subjectInfo['cipher'] = 'AES/CBC (RIJNDAEL) - 16Byte Key'
+                        writeHistoryFile( self.app.log_basename, self.subjectInfo )
+        
+                elif args[4] == 'header' and args[5] == 'ready':
+                    if self.app.config.get_setting('CogWorks Exp','subject_window'):
+                        self.app.gameevents.add("participant", "encrypted_rin", self.subjectInfo['encrypted_rin'], type='EVENT_SYSTEM')
+                    if self.expRoom and len(self.expRoom) > 0:
+                        self.app.gameevents.add("experiment", "room", self.expRoom, type='EVENT_SYSTEM')
+
+except ImportError:
+    pass
--- Plugins/PyViewX.py
+++ Plugins/PyViewX.py
@@ -4,100 +4,105 @@ PyViewX Integration
 import json
 import pygame
 import numpy as np
-from pyviewx import iViewXClient, Dispatcher
-from pyviewx.pygamesupport import Calibrator
 
-class SF5Plugin( object ):
+try:
+    from pyviewx import iViewXClient, Dispatcher
+    from pyviewx.pygamesupport import Calibrator
 
-    name = 'PyViewX'
-
-    d = Dispatcher()
-
-    header = ["gazetime", "gaze_type", "left_gaze_x", "right_gaze_x", "left_gaze_y", "right_gaze_y",
-              "left_pupil_x", "right_pupil_x", "left_pupil_y", "right_pupil_y",
-              "left_eye_x", "right_eye_x", "left_eye_y", "right_eye_y", "left_eye_z", "right_eye_z"]
-
-    logDrivers = [ 'Samples' ]
-
-    def __init__( self, app ):
-        super( SF5Plugin, self ).__init__()
-        self.app = app
-        self.client = None
-        self.post_calibrate_mode = -1
-        self.eye_data = None
-        self.null_data = ["NA"] * len( self.header )
-
-    def ready( self ):
-        if self.app.config.get_setting( self.name, 'enabled' ):
-            self.client = iViewXClient( self.app.config.get_setting( self.name, 'server_address' ), int( self.app.config.get_setting( 'PyViewX', 'server_outport' ) ) )
-            self.client.addDispatcher( self.d )
-            if self.client:
-                if self.app.config.get_setting( self.name, 'calmode' ) == 'Once':
-                    self.post_calibrate_mode = self.app.state
-                    self.app.state = self.app.STATE_CALIBRATE
-                self.app.reactor.listenUDP( int( self.app.config.get_setting( self.name, 'server_inport' ) ), self.client )
-                self.startDataStreaming()
-
-    def logHeader( self ):
-        if self.client:
-            return self.header
-        else:
-            return []
-
-    def logCallback( self ):
-        if self.eye_data:
-            return self.eye_data
-        else:
-            return self.null_data
-
-    @d.listen( 'ET_SPL' )
-    def iViewXEvent( self, inSender, inEvent, inResponse ):
-        self.eye_data = inResponse
-        if self.app.config.get_setting( 'Logging', 'logging' ) and self.app.config.get_setting( 'Logging', 'logDriver' ) == 'PyViewX:Samples':
-            self.app.log_world()
-
-    def startDataStreaming( self ):
-        self.client.setDataFormat( '%TS %ET %SX %SY %DX %DY %EX %EY %EZ' )
-        self.client.startDataStreaming()
-
-    def stopDataStreaming( self ):
-        self.client.stopDataStreaming()
-
-    def calibrationDone( self, lc, results, post_calibrate_mode ):
-        self.app.gameevents.add( "calibration", "results", "'%s'" % json.dumps(results, encoding="cp1252"), type = 'EVENT_SYSTEM' )
-        self.app.gameevents.add( "calibration", "stop", "", type = 'EVENT_SYSTEM' )
-        self.app.state = post_calibrate_mode
-
-    def calibrate( self, changeState ):
-        self.startDataStreaming()
-        if changeState:
-            self.post_calibrate_mode = self.app.state
-            self.app.state = self.app.STATE_CALIBRATE
-        self.app.gameevents.add( "calibration", "start", "", type = 'EVENT_SYSTEM' )
-        self.calibrator.start( self.calibrationDone, self.post_calibrate_mode )
-
-    def eventCallback( self, *args, **kwargs ):
-
-        if args[3] == 'config' and args[4] == 'load':
-
-            if args[5] == 'defaults':
-
-                self.app.config.add_setting( self.name, 'enabled', False, type = 2, alias = "Enable", about = 'Enable eye tracking using PyViewX' )
-                self.app.config.add_setting( self.name, 'server_address', '127.0.0.1', type = 3, alias = "Address", about = 'iViewX Server Address' )
-                self.app.config.add_setting( self.name, 'server_inport', '5555', type = 3, alias = "Incoming Port", about = 'iViewX Server Incoming Port' )
-                self.app.config.add_setting( self.name, 'server_outport', '4444', type = 3, alias = "Outgoing Port", about = 'iViewX Server Outgoing Port' )
-                self.app.config.add_setting( self.name, 'calmode', 'Once', type = 1, alias = 'When To Calibrate', options = ['Every Game', 'Once'], about = 'Set when eye tracker is calibrated' )
-
-        elif args[3] == 'display':
-
-            if args[4] == 'setmode':
+    class SF5Plugin( object ):
+    
+        name = 'PyViewX'
+    
+        d = Dispatcher()
+    
+        header = ["gazetime", "gaze_type", "left_gaze_x", "right_gaze_x", "left_gaze_y", "right_gaze_y",
+                  "left_pupil_x", "right_pupil_x", "left_pupil_y", "right_pupil_y",
+                  "left_eye_x", "right_eye_x", "left_eye_y", "right_eye_y", "left_eye_z", "right_eye_z"]
+    
+        logDrivers = [ 'Samples' ]
+    
+        def __init__( self, app ):
+            super( SF5Plugin, self ).__init__()
+            self.app = app
+            self.client = None
+            self.post_calibrate_mode = -1
+            self.eye_data = None
+            self.null_data = ["NA"] * len( self.header )
+    
+        def ready( self ):
+            if self.app.config.get_setting( self.name, 'enabled' ):
+                self.client = iViewXClient( self.app.config.get_setting( self.name, 'server_address' ), int( self.app.config.get_setting( 'PyViewX', 'server_outport' ) ) )
+                self.client.addDispatcher( self.d )
                 if self.client:
-                    self.calibrator = Calibrator( self.client, self.app.screen, reactor = self.app.reactor )
                     if self.app.config.get_setting( self.name, 'calmode' ) == 'Once':
-                        self.app.reactor.callLater( 1, self.calibrate, False )
+                        self.post_calibrate_mode = self.app.state
+                        self.app.state = self.app.STATE_CALIBRATE
+                    self.app.reactor.listenUDP( int( self.app.config.get_setting( self.name, 'server_inport' ) ), self.client )
+                    self.startDataStreaming()
+    
+        def logHeader( self ):
+            if self.client:
+                return self.header
+            else:
+                return []
+    
+        def logCallback( self ):
+            if self.eye_data:
+                return self.eye_data
+            else:
+                return self.null_data
+    
+        @d.listen( 'ET_SPL' )
+        def iViewXEvent( self, inSender, inEvent, inResponse ):
+            self.eye_data = inResponse
+            if self.app.config.get_setting( 'Logging', 'logging' ) and self.app.config.get_setting( 'Logging', 'logDriver' ) == 'PyViewX:Samples':
+                self.app.log_world()
+    
+        def startDataStreaming( self ):
+            self.client.setDataFormat( '%TS %ET %SX %SY %DX %DY %EX %EY %EZ' )
+            self.client.startDataStreaming()
+    
+        def stopDataStreaming( self ):
+            self.client.stopDataStreaming()
+    
+        def calibrationDone( self, lc, results, post_calibrate_mode ):
+            self.app.gameevents.add( "calibration", "results", "'%s'" % json.dumps(results, encoding="cp1252"), type = 'EVENT_SYSTEM' )
+            self.app.gameevents.add( "calibration", "stop", "", type = 'EVENT_SYSTEM' )
+            self.app.state = post_calibrate_mode
+    
+        def calibrate( self, changeState ):
+            self.startDataStreaming()
+            if changeState:
+                self.post_calibrate_mode = self.app.state
+                self.app.state = self.app.STATE_CALIBRATE
+            self.app.gameevents.add( "calibration", "start", "", type = 'EVENT_SYSTEM' )
+            self.calibrator.start( self.calibrationDone, self.post_calibrate_mode )
+    
+        def eventCallback( self, *args, **kwargs ):
+    
+            if args[3] == 'config' and args[4] == 'load':
+    
+                if args[5] == 'defaults':
+    
+                    self.app.config.add_setting( self.name, 'enabled', False, type = 2, alias = "Enable", about = 'Enable eye tracking using PyViewX' )
+                    self.app.config.add_setting( self.name, 'server_address', '127.0.0.1', type = 3, alias = "Address", about = 'iViewX Server Address' )
+                    self.app.config.add_setting( self.name, 'server_inport', '5555', type = 3, alias = "Incoming Port", about = 'iViewX Server Incoming Port' )
+                    self.app.config.add_setting( self.name, 'server_outport', '4444', type = 3, alias = "Outgoing Port", about = 'iViewX Server Outgoing Port' )
+                    self.app.config.add_setting( self.name, 'calmode', 'Once', type = 1, alias = 'When To Calibrate', options = ['Every Game', 'Once'], about = 'Set when eye tracker is calibrated' )
+    
+            elif args[3] == 'display':
+    
+                if args[4] == 'setmode':
+                    if self.client:
+                        self.calibrator = Calibrator( self.client, self.app.screen, reactor = self.app.reactor )
+                        if self.app.config.get_setting( self.name, 'calmode' ) == 'Once':
+                            self.app.reactor.callLater( 1, self.calibrate, False )
+    
+            elif args[3] == 'game':
+    
+                if args[4] == 'ready':
+                    if self.client and self.app.config.get_setting( self.name, 'calmode' ) == 'Every Game':
+                        self.calibrate( True )
 
-        elif args[3] == 'game':
-
-            if args[4] == 'ready':
-                if self.client and self.app.config.get_setting( self.name, 'calmode' ) == 'Every Game':
-                    self.calibrate( True )
+except ImportError:
+    pass
