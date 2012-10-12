3a0c9a40ca47fb888517dffb3725f627beda0b6f
Ryan Hope <rmh3093@gmail.com>
2012-10-12 01:33:37 -0400

    Delete cruft

--- Readme.rst
+++ Readme.rst
@@ -1,1 +1,6 @@
-README
+Requires:
+  1) PyGame
+  2) NumPy
+  3) Argparse
+
+easy_install argparse
--- version.py
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env python -O
-
-__version__ = "5.0.1_beta1"
-
-if __name__ == '__main__':
-    print __version__
