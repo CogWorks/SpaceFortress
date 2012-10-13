579f577125cb049c112332c10651b285fea89e53
Ryan Hope <rmh3093@gmail.com>
2012-10-13 11:27:58 -0400

    Fix PySide build for OSX

--- misc/setup_Config_Editor.py
+++ misc/setup_Config_Editor.py
@@ -1,5 +1,19 @@
 from bbfreeze import Freezer
-import shutil, glob, os, subprocess
+import shutil, glob, os, sys, subprocess
+
+def find_n_fix_missing(libs):
+    missing = []
+    for f in libs:
+        output = subprocess.Popen(['otool', '-XL', f], stdout=subprocess.PIPE).communicate()[0]
+        for line in output.splitlines():
+            libpath = line.strip().split(" ")[0]
+            if libpath != f and libpath[0] != "/" and libpath[0] != "@":
+                if not libpath in missing and not '.framework' in libpath:
+                    missing.append(libpath)
+                    shutil.copy(os.path.join('/usr/lib', libpath), libpath)
+                subprocess.Popen(['install_name_tool', '-change', libpath, "@executable_path/" + libpath, f])
+    if missing:
+        find_n_fix_missing(missing)
 
 includes = []
 excludes = ['Tkinter', 'readline']
@@ -10,19 +24,6 @@ freezer.include_py = False
 freezer.use_compression = True
 freezer()
 
-os.chdir(freezer.distdir)
-
-pyside_libs = []
-for f in glob.glob("/usr/lib/libpyside-*") + glob.glob("/usr/lib/libshiboken-*"):
-    lib = os.path.split(f)[1]
-    shutil.copy(f, lib)
-    pyside_libs.append(lib)
-    
-pyside_libs += glob.glob("PySide.Qt*.so")
-for f in pyside_libs:
-    output = subprocess.Popen(['otool', '-XL', f], stdout=subprocess.PIPE).communicate()[0]
-    for line in output.splitlines():
-        libpath = line.strip().split(" ")[0]
-        if libpath != f and libpath[0] != "/" and libpath[0] != "@":
-            subprocess.Popen(['install_name_tool', '-change', libpath, "@executable_path/"+libpath, f])
-    
+if sys.platform == 'darwin':
+    os.chdir(freezer.distdir)
+    find_n_fix_missing(glob.glob("*.so") + glob.glob("*.dylib"))
