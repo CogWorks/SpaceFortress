3bda748c2f0b987e6b4eb0737c90bcd81e734dd0
Ryan Hope <rmh3093@gmail.com>
2012-10-12 20:02:48 -0400

    Add pyside deps to configeditor build

--- misc/setup_Config_Editor.py
+++ misc/setup_Config_Editor.py
@@ -1,4 +1,5 @@
 from bbfreeze import Freezer
+import shutil, glob, os
 
 includes = []
 excludes = ['Tkinter', 'readline']
@@ -8,3 +9,8 @@ freezer.addScript('src/configeditor.py', gui_only=True)
 freezer.include_py = False
 freezer.use_compression = True
 freezer()
+
+for f in glob.glob("/usr/lib/libpyside-*"):
+    shutil.copy(f, os.path.join(freezer.distdir, os.path.split(f)[1]))
+for f in glob.glob("/usr/lib/libshiboken-*"):
+    shutil.copy(f, os.path.join(freezer.distdir, os.path.split(f)[1]))
