72df81b5a9805c2a7d8ee8fd9919afc796ff118d
Ryan Hope <rmh3093@gmail.com>
2012-10-12 22:16:16 -0400

    Make config editor work when installed on bare machine

--- misc/setup_Config_Editor.py
+++ misc/setup_Config_Editor.py
@@ -1,5 +1,5 @@
 from bbfreeze import Freezer
-import shutil, glob, os
+import shutil, glob, os, subprocess
 
 includes = []
 excludes = ['Tkinter', 'readline']
@@ -10,7 +10,19 @@ freezer.include_py = False
 freezer.use_compression = True
 freezer()
 
-for f in glob.glob("/usr/lib/libpyside-*"):
-    shutil.copy(f, os.path.join(freezer.distdir, os.path.split(f)[1]))
-for f in glob.glob("/usr/lib/libshiboken-*"):
-    shutil.copy(f, os.path.join(freezer.distdir, os.path.split(f)[1]))
+os.chdir(freezer.distdir)
+
+pyside_libs = []
+for f in glob.glob("/usr/lib/libpyside-*") + glob.glob("/usr/lib/libshiboken-*"):
+    lib = os.path.split(f)[1]
+    shutil.copy(f, lib)
+    pyside_libs.append(lib)
+    
+pyside_libs += glob.glob("PySide.Qt*.so")
+for f in pyside_libs:
+    output = subprocess.Popen(['otool', '-XL', f], stdout=subprocess.PIPE).communicate()[0]
+    for line in output.splitlines():
+        libpath = line.strip().split(" ")[0]
+        if libpath != f and libpath[0] != "/" and libpath[0] != "@":
+            subprocess.Popen(['install_name_tool', '-change', libpath, "@executable_path/"+libpath, f])
+    
